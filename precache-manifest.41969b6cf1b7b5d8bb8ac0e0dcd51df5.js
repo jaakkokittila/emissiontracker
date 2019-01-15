274368b552ce7d163227d8bc289f9d85570c422f
Kittilanjaakko <kittilanjaakko@gmail.com>
2019-01-15 10:52:48 +0200

    Updates

---
+++ precache-manifest.41969b6cf1b7b5d8bb8ac0e0dcd51df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd0764c90b465c4a05313cf12e4ee11c",
+    "url": "/jaakkokittila/emissiontracker.git/static/media/API_SP.POP.TOTL_DS2_en_xml_v2_10224853.fd0764c9.xml"
+  },
+  {
+    "revision": "acf34dce43409bb175af5643595f2ebe",
+    "url": "/jaakkokittila/emissiontracker.git/static/media/API_EN.ATM.CO2E.KT_DS2_en_xml_v2_10227343.acf34dce.xml"
+  },
+  {
+    "revision": "c559f3f2534739678d89",
+    "url": "/jaakkokittila/emissiontracker.git/static/js/runtime~main.c559f3f2.js"
+  },
+  {
+    "revision": "59d3168db54dc4c415fe",
+    "url": "/jaakkokittila/emissiontracker.git/static/js/main.59d3168d.chunk.js"
+  },
+  {
+    "revision": "022e95d9a0afed8cdd01",
+    "url": "/jaakkokittila/emissiontracker.git/static/js/1.022e95d9.chunk.js"
+  },
+  {
+    "revision": "59d3168db54dc4c415fe",
+    "url": "/jaakkokittila/emissiontracker.git/static/css/main.5ff65c41.chunk.css"
+  },
+  {
+    "revision": "011487327d7efd0e636d4095427404ff",
+    "url": "/jaakkokittila/emissiontracker.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/jaakkokittila/emissiontracker.git/precache-manifest.41969b6cf1b7b5d8bb8ac0e0dcd51df5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/jaakkokittila/emissiontracker.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
